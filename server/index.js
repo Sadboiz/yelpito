2acf79a73f4e2afc1256f3ba87ab2464180594fd
RaulVinueza <raulmvinueza@gmail.com>
2018-03-18 16:55:48 -0500

    set up server

---
+++ server/index.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const app = express()
+const morgan = require('morgan')
+const path = require('path')
+const bodyParser = require('body-parser')
+const port = process.env.PORT || 3000
+
+app.use(morgan('dev'))
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: true}))
+app.use(express.static(path.join(__dirname, '../public')))
+
+app.get('*', function (req, res) {
+    res.sendFile(path.join(__dirname, '../public/index.html'))
+  })
+
+
+app.listen(port, function () {
+  console.log(`Now listening on port ${port}`);
+})
